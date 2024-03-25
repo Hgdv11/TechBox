d230d15b04d2b168039a9613bdafcd0c1f2f06b3
SantiagerArmendariz <140926069+SantiagerArmendariz@users.noreply.github.com>
2024-03-25 16:01:14 -0600

    Update CustomDrawer.js
    Agregué traducciones y botón para cambiar de idioma

--- Components/CustomDrawer.js
+++ Components/CustomDrawer.js
@@ -1,5 +1,5 @@
 import { View, Text, ImageBackground } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
 
@@ -8,8 +8,22 @@ import {
   DrawerItemList,
 } from "@react-navigation/drawer";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { useTranslation } from "react-i18next";
+
+import i18next from "i18next";
 
 const CustomDrawer = (props) => {
+  // Estado para mantener el idioma actual
+  const [currentLanguage, setCurrentLanguage] = useState("es");
+  const { t } = useTranslation();
+
+  const toggleLanguage = () => {
+    const newLanguage = currentLanguage === "es" ? "en" : "es";
+    setCurrentLanguage(newLanguage);
+    i18next.changeLanguage(newLanguage);
+    props.navigation.closeDrawer();
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <DrawerContentScrollView
@@ -22,12 +36,24 @@ const CustomDrawer = (props) => {
       </DrawerContentScrollView>
       <View style={{ padding: 20, borderTopWidth: 1, borderColor: "#ccc" }}>
         <TouchableOpacity
-          onPress={() => props.navigation.navigate("Register")}
+          onPress={toggleLanguage}
           style={{ paddingVertical: 15 }}
         >
           <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Ionicons name="language" size={22} />
+            <Text style={{ marginLeft: 10 }}>{t("drawer.switch")}</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => props.navigation.navigate("Register")}
+          style={{ paddingVertical: 15, marginTop: 10 }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
             <Ionicons name="exit" size={22} />
-            <Text style={{ marginLeft: 10 }}>Cerrar Sesión</Text>
+            <Text style={{ marginLeft: 10 }}>
+            {t("drawer.logout")}
+            </Text>
           </View>
         </TouchableOpacity>
       </View>
