0e2c27635377d50c30c171fda238710bc533370e
SantiagerArmendariz <140926069+SantiagerArmendariz@users.noreply.github.com>
2024-03-25 16:05:47 -0600

    Agregué el provider de la traducción

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import "react-native-gesture-handler";
+import { I18nextProvider } from "react-i18next";
+import i18n from './services/118next';
 
 import Navigation from "./Navigation";
 
 export default function App() {
   return (
-    <Navigation />
+    <I18nextProvider i18n={i18n}>
+      <Navigation />
+    </I18nextProvider>
   );
 }
